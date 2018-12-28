c205b879bc00527afd2900ded27139fe166a6123
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-27 19:09:04 -0600

    Loading local html, css and javascript inside th electron window.

--- index.js
+++ index.js
@@ -21,15 +21,41 @@ app.on('before-quit', () =>
 });
 
 // nuestra primera ventana
-app.on('ready',() =>
-{
-    // creando una ventana luego de que este lista la app
-    let win = new BrowserWindow();
+app.on('ready',() =>{
+    let win = new BrowserWindow({
+        // definiendo propiedades de la ventana
+        width: 1000,
+        height: 600,
+        title: "Zamus",
+        center: true,
+        // modificar el maximizable no esta implementado para linux
+        maximizable: false,
+        show: false
+    });
 
-    // ponemos a escuchar el evento de cierre de ventana
-    win.on('closed', () => {
-        // eliminando de la memoria el objeto y cerrando
+    // ejecutandose solo una vez
+    // esperamos la carga completa de un servicio remoto
+    win.once("ready-to-show", () => {
+        win.show();
+    });
+
+    // se ejecuta cada que la ventana es movida
+    win.on("move",() =>{
+        const position =win.getPosition();
+        console.log(`La posicion de la ventana es ${position}`);
+    });
+
+    // escuchando al cierre de la aplicacion
+    app.on("closed", () => {
+        // vaciando la memoria que contiene la ventana
         win = null;
+        // cerrando el aplicativo
         app.quit();
-    })
+    });
+
+    // cargando una web remota
+    //win.loadURL("http://devdocs.io/");
+    // cargando una web local, los archivos que forman parte de la
+    // misma carpeta que el proyecto
+    win.loadURL(`file://${__dirname}/index.html`);
 });
